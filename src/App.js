f03ef207b13a2b41f3ed0e37fb563a2e150897e1
Naveen <naveen.w3master@gmail.com>
2020-07-02 01:18:24 +0530

    Router for Login and Dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "admin-lte/dist/css/adminlte.css";
 import "./App.css";
+import DashboardPage from "./pages/DashboardPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/dashboard">
+          <DashboardPage />
+        </Route>
+        <Route path="/">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
